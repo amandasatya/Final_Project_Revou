fc1e796ad41dbb15af632115671fe2ec75717be9
haikalbintang <mhaikalbintang.work@gmail.com>
2024-05-10 22:26:56 +0700

    Add 'Our Team' to about-us.

--- Frontend/final-project-revou/src/app/about-us/page.tsx
+++ Frontend/final-project-revou/src/app/about-us/page.tsx
@@ -1,7 +1,17 @@
+import Footer_vmhb from "@/components/Footer_vmhb";
+import GroupProfile from "@/components/GroupProfile";
+import NavbarWrapper from "@/components/NavbarWrapper";
+import ProjectProfile from "@/components/ProjectProfile";
+
 export default function AboutUs() {
   return (
-    <>
-      <div></div>
-    </>
+    <div className="xl:container px-2">
+      <NavbarWrapper />
+      <ProjectProfile />
+      <div className="px-3">
+        <GroupProfile />
+      </div>
+      <Footer_vmhb />
+    </div>
   );
 }
